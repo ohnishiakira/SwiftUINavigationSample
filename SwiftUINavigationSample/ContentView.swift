5845c4eda8d3d73b26aa013a707f80c4fc05a42c
Akira Ohnishi <s06206ao@gmail.com>
2022-02-15 09:06:05 +0900

    Initial Commit

---
+++ SwiftUINavigationSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigationSample
+//
+//  Created by Akira Ohnishi on 2022/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINavigationSample/SwiftUINavigationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationSampleApp.swift
+//  SwiftUINavigationSample
+//
+//  Created by Akira Ohnishi on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
